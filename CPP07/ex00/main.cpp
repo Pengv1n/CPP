91c7e6cc3d2c7bf935248f40595313a53b2ec420
Akira Regenia <aregenia@at-o9.msk.21-school.ru>
2022-10-23 16:38:30 +0300

    start cpp07

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Akira Regenia on 10/23/22.
+//
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Akira Regenia on 10/23/22.
+//
+
+#ifndef CPP_WHATEVER_H
+#define CPP_WHATEVER_H
+
+
+class whatever {
+
+};
+
+
+#endif //CPP_WHATEVER_H
